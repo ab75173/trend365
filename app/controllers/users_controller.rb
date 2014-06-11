c1e4673188038f15762eb38021f05c857ced67d6
maryhipp <maryhipp@gmail.com>
2014-06-11 16:41:33 -0400

    User can change password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,30 @@
 class UsersController < ApplicationController
 
-def new
-  @user = User.new
-end
-
-def create
-  @user = User.new(user_params)
-  if @user.save
-    redirect_to root_url, :notice => "Signed up!"
-  else
-    render :new
+  def new
+    @user = User.new
   end
-end
 
-private
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_url, :notice => "Signed up!"
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to root_path
+  end
+
+  private
+
   def user_params
     params.require(:user).permit(
       :email,
