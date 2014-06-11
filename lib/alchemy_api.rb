714f25cf1721857db624a42a617abd69fbb1b551
yihsiaol <yihsiaol@me.com>
2014-06-11 14:53:31 -0400

    add attribution link

--- lib/alchemy_api.rb
+++ lib/alchemy_api.rb
@@ -1,3 +1,6 @@
+# handles Alchemy API calls and responses
+# Docs: http://www.alchemyapi.com/api/
 class AlchemyAPI
-  
+  # base url for Alchemy API
+  BASE_URL = "http://access.alchemyapi.com/"
 end
