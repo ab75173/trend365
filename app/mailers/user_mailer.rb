412a79a5f2ec94717d67ad0be6544b12b9a8ee2c
richessler <richessler@gmail.com>
2014-06-11 23:43:27 -0400

    Set up ActionMailer on create and reset PW, also set up ENVs for our email address for Trend365 - info in .env

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,21 @@
 class UserMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "trend365@wdidc01.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.user_mailer.reset_password_email.subject
   #
+
+  def signup_confirmation(user)
+    @user = user
+    @greeting = "Hi"
+
+    mail to:  user.email, subject: "Sign Up Confirmation" do |format|
+      format.html { render 'signup_confirmation'}
+    end
+  end
+
   def reset_password_email(user)
     @user = user
     @url  = edit_password_reset_url(user.reset_password_token)
