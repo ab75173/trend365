095519ad344d1b7e7fc98a75527eb15aaba36fae
richessler <richessler@gmail.com>
2014-06-11 23:43:27 -0400

    Set up ActionMailer on create and reset PW, also set up ENVs for our email address for Trend365 - info in .env

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,12 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# config.action_mailer.smtp_settings = {
+#   enable_starttls_auto: true,
+#   address: 'smtp.gmail.com',
+#   port: 587,
+#   authentication: 'plain',
+#   user_name: 'myemail@gmail.com',
+#   password: '<password>'
+# }
