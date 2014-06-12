0ca7ada7ce138d52976d49e76b7fee4a52f8387b
yihsiaol <yihsiaol@me.com>
2014-06-12 11:16:59 -0400

    User can now see details about a specific article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,22 +6,36 @@ class ArticlesController < ApplicationController
   def search
     @keyword = params[:article][:name]
     nyt_response = get_search_results @keyword
-    binding.pry
 
     # Grabs the headline and ID from each article search result
     @results = nyt_response.map do |article|
-      {
-        headline: article["headline"]["main"],
-        id: article["_id"]
-      }
+      { id: article["_id"] }
     end
   end
 
   def show
+    nyt_response = get_specific_article(params[:id])[0]
 
+    @result = {
+      :headline => nyt_response["headline"]["main"],
+      :snippet  => nyt_response["snippet"],
+      :url      => nyt_response["web_url"],
+    }
   end
 
   private
+  def get_specific_article(id)
+    base_url = "http://api.nytimes.com/svc/search/v2/articlesearch.json"
+    params = {
+      :fq        => "_id:(\"#{id}\")",
+      :"api-key" => Rails.application.secrets[:nyt_api_key]
+    }
+    request_url = [base_url, params.to_query].join("?")
+
+    results = HTTParty.get request_url
+
+    return results["response"]["docs"]
+  end
 
   def get_search_results(keyword)
     keyword = keyword.gsub(' ', '%20')
